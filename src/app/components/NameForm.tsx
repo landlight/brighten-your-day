c4c7bfcf9b33a0d178d3e7ddf1aa41df49ab07f6
Chit Khine (Light) <light@townbase.com>
2024-09-25 11:27:23 +0700

    :sparkles: add name

--- src/app/components/NameForm.tsx
+++ src/app/components/NameForm.tsx
@@ -1,15 +1,38 @@
-import { Box, Button, Input } from "@mui/material";
+import { Box, Button, TextField } from "@mui/material";
 import { useState } from "react";
 
 export interface NameFormProps {
-    save: (input: string) => void;
+  save: (input: string) => void;
 }
 export const NameForm: React.FC<NameFormProps> = ({ save }: NameFormProps) => {
-    const [name, setName] = useState("");
-    return (
-        <Box style={{ background: "white", display: "flex", flexDirection: "column", padding: "10px", borderRadius: "10px", width: "300px"}}> 
-            <Input placeholder="Please enter your nickname" onChange={(e) => setName(e.target.value)} />
-            <Button style={{marginTop: "10px"}} variant="contained" onClick={() => save(name)}>Save</Button>
-        </Box>
-    );
-}
+  const [name, setName] = useState("");
+  return (
+    <Box
+      style={{
+        background: "white",
+        display: "flex",
+        flexDirection: "column",
+        padding: "10px",
+        borderRadius: "10px",
+        width: "300px",
+      }}
+    >
+      <TextField
+        placeholder="Please enter your nickname"
+        onChange={(e) => setName(e.target.value)}
+        onKeyDown={(event) => {
+          if (event.key === "Enter") {
+            save(name);
+          }
+        }}
+      />
+      <Button
+        style={{ marginTop: "10px" }}
+        variant="contained"
+        onClick={() => save(name)}
+      >
+        Save
+      </Button>
+    </Box>
+  );
+};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,15 +2,33 @@
 
 import styles from "./page.module.css";
 import { CardBox } from "./components/CardBox";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { NameForm } from "./components/NameForm";
 
 export default function Home() {
-  let [ name, setName ] = useState(localStorage.getItem("name"));
+  let [name, setName] = useState("");
+  let [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    let value = localStorage.getItem("name") || "";
+    setName(value);
+    setLoading(false);
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <main className={styles.main}>
-      {!name && <NameForm save={(input) => {setName(input); localStorage.setItem("name", input)}}/>}
+      {!name && (
+        <NameForm
+          save={(input) => {
+            setName(input);
+            localStorage.setItem("name", input);
+          }}
+        />
+      )}
       {name && <CardBox title="Welcome" description={`Hello, ${name}!`} />}
     </main>
   );
